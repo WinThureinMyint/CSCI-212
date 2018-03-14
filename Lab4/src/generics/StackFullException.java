379253ffb5d66b6af6a9c95ff570668d63d3477d
Win Thurein Myint <harrychan2139@gmail.com>
2018-03-14 19:13:42 -0400

    Lab 4

---
+++ Lab4/src/generics/StackFullException.java
@@ -0,0 +1,17 @@
+package generics;
+
+public class StackFullException extends RuntimeException {
+	/**
+	 * Constructs a new StackFullException with a default error message string.
+	 */
+	public StackFullException(){
+		super("Exception : Stack is full");
+	}
+	/**
+	 * Constructs a new StackFullException with the parameter as the error message string.
+	 * @param msg The string passed as the error message string.
+	 */
+	public StackFullException(String msg){
+		super(msg);
+	}
+}
