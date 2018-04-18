b996659479b413dcc3a68a69dda569656b34193e
Win Thurein Myint <harrychan2139@gmail.com>
2018-04-18 19:25:16 -0400

    Add files via upload

---
+++ Lab6/src/lab6a/Lab6aApp.java
@@ -0,0 +1,71 @@
+package lab6a;
+
+/**
+ * <p>Title: Driver Class - Lab6aApp</p>
+ *
+ * <p>Copyright: Copyright (c) 2018</p>
+ *
+ * @author A. Abreu
+ * @version 1.0
+ */
+import java.sql.*;
+
+public class Lab6aApp {
+
+	/**
+	 * getData method -- gets the products from an SQLite database
+	 * @return the an array of products
+	 */
+	public static Product[] getData(){		
+		Statement stmt = null;
+		int records = 0;
+		Product[] products = null; 
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection c = DriverManager.getConnection("jdbc:sqlite:I:/aabreu/products.db");
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully");			
+
+			stmt = c.createStatement();
+			
+			ResultSet rs = stmt.executeQuery("SELECT * FROM products;");
+			ResultSetMetaData rsmd = rs.getMetaData();			
+			
+			for(int i = 1; i <= rsmd.getColumnCount(); i++)	{
+				System.out.print(String.format("%-12s", rsmd.getColumnLabel(i)) + "\t");
+				System.out.print(rsmd.getColumnTypeName(i) + "\t");
+				System.out.println(rsmd.getPrecision(i));
+			}
+			
+			rs = stmt.executeQuery("select count (*) AS totalRecords from products");
+			int totalRecords = rs.getInt("totalRecords");
+			System.out.println("Records: " + totalRecords);
+			
+			rs = stmt.executeQuery("SELECT * FROM products;");
+			if(rs != null){
+				products = new Product[totalRecords];
+				while (rs.next()) {
+					String prodId = rs.getString("prodId");
+					int quantity = rs.getInt("quantity");
+					double price = rs.getFloat("price");
+
+					System.out.println(String.format("%3s %-6s %3d %6.2f",
+							records, prodId, quantity, price));	
+					products[records++] = new Product(prodId, quantity, price);
+				}
+				System.out.println();
+			}
+			
+			stmt.close();
+			c.commit();
+			c.close();
+		} 
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch(SQLException se){
+			System.err.println(se.getClass().getName() + ": " + se.getMessage());
+		}
+		return products;
+	}
+}
